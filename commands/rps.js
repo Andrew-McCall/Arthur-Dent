50ff974bbab3a28390a6a11fab7a9d5ddfd6bebb
Selene <leonorcdmd@proton.me>
2025-01-24 21:01:06 +0100

    I love Marcel

--- commands/rps.js
+++ commands/rps.js
@@ -2,10 +2,10 @@ import { SlashCommandBuilder } from 'discord.js';
 
 export default {
 	data: new SlashCommandBuilder()
-		.setName('ttt')
-		.setDescription('Tik-Tak-Toe.')
+		.setName('rps')
+		.setDescription('Rock Paper Scissors')
 		.addStringOption(option=>option.setName("blowjob").setDescription("are fun").setRequired(true)),
 	async execute(interaction) {
-		await interaction.reply("`Hello World.`"+interaction.options.getString("blowjob")+Math.random());
+		await interaction.reply("`Hello World.`"+interaction.options.getString("blowjob")+M);
 	},
 };
