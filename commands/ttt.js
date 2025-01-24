bdd3215f79fa16040c65e48f47416a9c43139faa
Selene <leonorcdmd@proton.me>
2025-01-24 20:46:27 +0100

    Fixed the blowjob

--- commands/ttt.js
+++ commands/ttt.js
@@ -4,7 +4,7 @@ export default {
 	data: new SlashCommandBuilder()
 		.setName('ttt')
 		.setDescription('Tik-Tak-Toe.')
-		.setStringOption(option=>option.setName("blowjob").setDescription("are fun").setRequired(true)),
+		.addStringOption(option=>option.setName("blowjob").setDescription("are fun").setRequired(true)),
 	async execute(interaction) {
 		await interaction.reply("`Hello World.`"+interaction.options.getString("blowjob"));
 	},
