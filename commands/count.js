80e17b086791e78dece359cd4d0b6144ae2433ff
Selene <leonorcdmd@proton.me>
2025-01-24 20:32:55 +0100

    Added OMG THAT SO BIG

--- commands/count.js
+++ commands/count.js
@@ -1,11 +1,18 @@
 import { SlashCommandBuilder } from 'discord.js';
-let count=0;
+let count = 0;
 export default {
 	data: new SlashCommandBuilder()
 		.setName('count')
 		.setDescription('Test command.'),
 	async execute(interaction) {
-		count=count+1
-		await interaction.reply(count+"poop");
+		count = count + 1
+
+		if (count > 10) {
+			await interaction.reply("OMG THAT SOOOOOO BIG")
+
+		} else {
+			await interaction.reply(count + " poop");
+		}
+
 	},
 };
